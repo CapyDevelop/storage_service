f5db044475239954d0e7050fa27ebd73def877df
Alexsei Popov <popov2895@gmail.com>
2024-01-03 16:53:28 +0300

    change aws settings into environment

--- main.py
+++ main.py
@@ -34,7 +34,11 @@ class StorageService(storage_pb2_grpc.StorageServiceServicer):
             with Image.open(filename) as im:
                 im.save(filename_webp, format="webp")
             os.remove(filename)
-        session = boto3.session.Session()
+        session = boto3.session.Session(
+            aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+            aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
+            region_name=os.getenv("AWS_REGION_NAME")
+        )
         s3 = session.client(
             service_name='s3',
             endpoint_url='https://storage.yandexcloud.net',
